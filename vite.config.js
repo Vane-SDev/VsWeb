9dbc1fea2ab6352680514dc63c5525e6fdabda20
Vane-SDeV <vanesa.soria.p@gmail.com>
2025-07-28 01:20:41 -0300

    txt

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,27 @@
-// vite.config.js
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import sitemap from "vite-plugin-sitemap"; 
+import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
   plugins: [
     react(),
     sitemap({
       hostname: "https://www.vswebdesign.online",
+      generateRobotsTxt: true,
       robots: [
         {
           userAgent: "*",
           allow: "/",
         },
       ],
-
-      dynamicRoutes: ["/servicios", "/proyectos", "/testimonios", "/contacto"],
+      dynamicRoutes: [
+        "/",
+        "/servicios",
+        "/proyectos",
+        "/testimonios",
+        "/contacto",
+      ],
+      exclude: ["/syjxlwfuc9abb9kq9v80wlcyk57d9l"],
     }),
   ],
 });
